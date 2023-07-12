7713744bfcbc7cd592244d3d5420d63dfa3bc0a3
Enes Gelmez <enes.gelmez@hotmail.com>
2023-07-12 16:45:28 +0300

    Created sender and receivers to the project

---
+++ RabbitMQReceiver1/Program.cs
@@ -0,0 +1,49 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+
+ConnectionFactory factory = new ConnectionFactory();
+factory.Uri = new Uri(uriString: "amqp://guest:guest@localhost:5672");
+factory.ClientProvidedName = "Rabbit Receiver App 1";
+
+
+IConnection connection = factory.CreateConnection();
+IModel model = connection.CreateModel();
+
+string exchangeName = "DemoExchange";
+string routingKey = "demo-routing-key";
+string queueName = "DemoQueue";
+
+model.ExchangeDeclare(exchangeName, ExchangeType.Direct);
+model.QueueDeclare(queueName, durable: false, exclusive: false, autoDelete: false, arguments: null);
+model.QueueBind(queueName, exchangeName, routingKey, arguments: null);
+model.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+
+var consumer = new EventingBasicConsumer(model);
+consumer.Received += Consumer_Received;
+
+string consumerTag = model.BasicConsume(queueName, autoAck: false, consumer);
+Console.ReadLine();
+model.BasicCancel(consumerTag);
+model.Close();
+connection.Close();
+
+
+void Consumer_Received(object? sender, BasicDeliverEventArgs e)
+{
+    Task.Delay(TimeSpan.FromSeconds(5)).Wait();
+    var body = e.Body.ToArray();
+    string message = Encoding.UTF8.GetString(body);
+
+    if (!message.Contains("12"))
+    {
+        Console.WriteLine(value: $"Message Received: {message}");
+        model.BasicAck(e.DeliveryTag, multiple: false);
+    }
+    else
+    {
+        Console.WriteLine(value: $"Message Rejected: {message}");
+        model.BasicNack(e.DeliveryTag, multiple: false, requeue: false);
+    }
+}
---
+++ RabbitMQSender/Program.cs
@@ -0,0 +1,30 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+
+ConnectionFactory factory = new ConnectionFactory();
+factory.Uri = new Uri(uriString: "amqp://guest:guest@localhost:5672");
+factory.ClientProvidedName = "Rabbit Sender App";
+
+
+IConnection connection = factory.CreateConnection();
+IModel model = connection.CreateModel();
+
+string exchangeName = "DemoExchange";
+string routingKey = "demo-routing-key";
+string queueName = "DemoQueue";
+
+model.ExchangeDeclare(exchangeName, ExchangeType.Direct);
+model.QueueDeclare(queueName, durable: false, exclusive: false, autoDelete: false, arguments: null);
+model.QueueBind(queueName, exchangeName, routingKey, arguments: null);
+
+for (int i = 1; i <= 60; i++)
+{
+    Console.WriteLine($"Sending Message {i}");
+    byte[] messageBodyBytes = Encoding.UTF8.GetBytes($"Message  #{i}");
+    model.BasicPublish(exchangeName, routingKey, basicProperties: null, messageBodyBytes);
+    Thread.Sleep(1000);
+}
+
+model.Close();
+connection.Close();
